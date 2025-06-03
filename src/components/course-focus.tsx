4328590dc4b452d6886d772deb421f6e8f5bb1c4
Tim Toller <tim.m.toller+github@gmail.com>
2025-06-03 09:17:25 +0200

    Revert "WIP Course priority"
    This reverts commit 8644680d10e40e17dd4d79d8174df825097a5bba.

--- src/components/course-focus.tsx
+++ src/components/course-focus.tsx
@@ -1,25 +1,9 @@
-import { priorityAtom } from "@/store/priority";
-import { useAtom } from "jotai";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
-
 export default function CourseFocus() {
-	const [priorities] = useAtom(priorityAtom);
-	console.log("Course Focus priorities:", priorities);
-
 	return (
 		<div>
 			<h2 className="text-2xl font-bold">Course Focus</h2>
-			What courses should you focus on must in your studies? Which courses will have the biggest effect on your overall grade?
-			<Accordion type="single" collapsible className="w-full">
-				{priorities.map((priority) => (
-					<AccordionItem key={priority.name} value={priority.name}>
-						<AccordionTrigger className="text-lg font-semibold">{priority.name}</AccordionTrigger>
-						<AccordionContent>
-							<p>{priority.explanation}</p>
-						</AccordionContent>
-					</AccordionItem>
-				))}
-			</Accordion>
+			What courses should you focus on must in your studies? Which courses will have the biggest effect on your overall grade? For
+			each course we calculate an "importance score" which is defined as:
 		</div>
 	);
 }
--- src/screens/grades-screen.tsx
+++ src/screens/grades-screen.tsx
@@ -18,80 +18,77 @@ export default function GradesScreen() {
 	const [{ distinction, moreThanHalfOne, noThree }] = useAtom(passedWithDistinctionAtom);
 
 	return (
-		<>
-			<section className="h-full grid gap-4 grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 grid-rows-4 m-4">
+		<section className="h-full grid gap-4 grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 grid-rows-4 m-4">
+			<Card>
+				<CardHeader>
+					<CardTitle>Average</CardTitle>
+				</CardHeader>
+				<CardContent className="flex flex-col gap-4">
+					<div className="flex flex-row justify-between">
+						<h2 className="text-lg">Course Average:</h2>
+						<h3 className="text-lg font-bold">{(courseGradeAverage && round(courseGradeAverage)) || "-"}</h3>
+					</div>
+					<div className="flex flex-row justify-between">
+						<h2 className="text-lg">Group Average:</h2>
+						<h3 className="text-lg font-bold">
+							{(groupGradesRounded.length && round(average(groupGradesRounded.map((g) => g.average).filter((g) => !isNaN(g))))) ||
+								"-"}
+						</h3>
+					</div>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader>
+					<CardTitle>Distinction</CardTitle>
+				</CardHeader>
+				<CardContent className="flex flex-col gap-4">
+					<div className="flex flex-row justify-between">
+						<h2 className="text-lg">No Grade 3:</h2>
+						<h3 className="text-lg font-bold">{noThree ? "TRUE" : "FALSE"}</h3>
+					</div>
+					<div className="flex flex-row justify-between">
+						<h2 className="text-lg">More than half Grade 1:</h2>
+						<h3 className="text-lg font-bold">{moreThanHalfOne ? "TRUE" : "FALSE"}</h3>
+					</div>
+					<div className="flex flex-row justify-between">
+						<h2 className="text-lg">Passed with distinction</h2>
+						<h3 className="text-lg font-bold">{distinction ? "TRUE" : "FALSE"}</h3>
+					</div>
+				</CardContent>
+			</Card>
+			{groupGradesAverage.map(({ average, gradedECTS, name, totalECTS }) => (
 				<Card>
 					<CardHeader>
-						<CardTitle>Average</CardTitle>
-					</CardHeader>
-					<CardContent className="flex flex-col gap-4">
-						<div className="flex flex-row justify-between">
-							<h2 className="text-lg">Course Average:</h2>
-							<h3 className="text-lg font-bold">{(courseGradeAverage && round(courseGradeAverage)) || "-"}</h3>
+						<div className="flex flex-col gap-2">
+							<div className="flex justify-between items-center">
+								<span className="text-sm font-medium">ECTS</span>
+								<span className="text-sm font-medium">
+									{gradedECTS}/{totalECTS} ({round((gradedECTS / totalECTS) * 100)}%)
+								</span>
+							</div>
+							<Progress
+								value={round((gradedECTS / totalECTS) * 100)}
+								className={"w-full"}
+								indicatorColor={(gradedECTS >= totalECTS && "bg-green-500") || undefined}
+							/>
 						</div>
-						<div className="flex flex-row justify-between">
-							<h2 className="text-lg">Group Average:</h2>
-							<h3 className="text-lg font-bold">
-								{(groupGradesRounded.length &&
-									round(average(groupGradesRounded.map((g) => g.average).filter((g) => !isNaN(g))))) ||
-									"-"}
-							</h3>
+						<CardTitle className="text-lg">{name}</CardTitle>
+					</CardHeader>
+					<CardContent className="flex flex-col gap-4 justify-between">
+						<div className="">
+							<div className="flex flex-row justify-between">
+								<h2 className="text-lg">Average Grade:</h2>
+								<h3 className="text-lg font-bold">{round(average) || "-"}</h3>
+							</div>
+							<div className="flex flex-row justify-between">
+								<h2 className="text-lg">Rounded Grade:</h2>
+								<h3 className="text-lg font-bold">{round(average, 0) || "-"}</h3>
+							</div>
 						</div>
 					</CardContent>
 				</Card>
-				<Card>
-					<CardHeader>
-						<CardTitle>Distinction</CardTitle>
-					</CardHeader>
-					<CardContent className="flex flex-col gap-4">
-						<div className="flex flex-row justify-between">
-							<h2 className="text-lg">No Grade 3:</h2>
-							<h3 className="text-lg font-bold">{noThree ? "TRUE" : "FALSE"}</h3>
-						</div>
-						<div className="flex flex-row justify-between">
-							<h2 className="text-lg">More than half Grade 1:</h2>
-							<h3 className="text-lg font-bold">{moreThanHalfOne ? "TRUE" : "FALSE"}</h3>
-						</div>
-						<div className="flex flex-row justify-between">
-							<h2 className="text-lg">Passed with distinction</h2>
-							<h3 className="text-lg font-bold">{distinction ? "TRUE" : "FALSE"}</h3>
-						</div>
-					</CardContent>
-				</Card>
-				{groupGradesAverage.map(({ average, gradedECTS, name, totalECTS }) => (
-					<Card>
-						<CardHeader>
-							<div className="flex flex-col gap-2">
-								<div className="flex justify-between items-center">
-									<span className="text-sm font-medium">ECTS</span>
-									<span className="text-sm font-medium">
-										{gradedECTS}/{totalECTS} ({round((gradedECTS / totalECTS) * 100)}%)
-									</span>
-								</div>
-								<Progress
-									value={round((gradedECTS / totalECTS) * 100)}
-									className={"w-full"}
-									indicatorColor={(gradedECTS >= totalECTS && "bg-green-500") || undefined}
-								/>
-							</div>
-							<CardTitle className="text-lg">{name}</CardTitle>
-						</CardHeader>
-						<CardContent className="flex flex-col gap-4 justify-between">
-							<div className="">
-								<div className="flex flex-row justify-between">
-									<h2 className="text-lg">Average Grade:</h2>
-									<h3 className="text-lg font-bold">{round(average) || "-"}</h3>
-								</div>
-								<div className="flex flex-row justify-between">
-									<h2 className="text-lg">Rounded Grade:</h2>
-									<h3 className="text-lg font-bold">{round(average, 0) || "-"}</h3>
-								</div>
-							</div>
-						</CardContent>
-					</Card>
-				))}
-			</section>
+			))}
 			<CourseFocus />
-		</>
+		</section>
 	);
 }
